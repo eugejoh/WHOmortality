58b385f60664728f94688011a2f4e394de2adeee
eugejoh <ejoh@bu.edu>
2019-10-23 16:16:03 -0400

    added roxygen documentation

--- R/get_dd.R
+++ R/get_dd.R
@@ -1,3 +1,19 @@
+#' Import Data Dictionary for WHO Mortality and Population Tables
+#' 
+#' This function imports the data dictionaries for the mortality and population tables. These were extracted from the documentation word document "Documentation_1May2019.doc".
+#'
+#' @param type a single \code{character} specifying to import either the mortality or population data dictionary
+#' @param ... other parameters pass to \code{\link{read_csv}}.
+#'
+#' @return a \code{data.frame} of the data dictionary with mappings of field names and descriptions
+#' 
+#' @export
+#'
+#' @examples
+#' \dontrun{
+#' get_dd(type = "mort")
+#' get_dd(type = "pop")
+#' }
 get_dd <- function(type = c("mort", "pop"), ...) {
   csv_files <- list.files(here::here("data"),
                           pattern = "\\.csv$",
@@ -12,5 +28,3 @@ get_dd <- function(type = c("mort", "pop"), ...) {
   
   readr::read_csv(dd, ...)
 }
-
-# get_dd(type = "mort")
