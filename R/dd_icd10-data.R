74a21622def05bbf2b069336a1adb2a6135b569c
eugejoh <ejoh@bu.edu>
2019-10-28 08:48:26 -0400

    added icd-10 data dict

---
+++ R/dd_icd10-data.R
@@ -0,0 +1,25 @@
+#' ICD-10 Mortality Data Dictionary
+#'
+#' Data dictionary for the ICD-10 codes used in the mortality table, extracted from Table 8 in the
+#' documentation Word document "Documentation_1May2019.doc"
+#'
+#' @docType data
+#'
+#' @usage data(dd_icd10)
+#'
+#' @format A \code{data.frame} object containing the column names and descriptions
+#' for the ICD-10 code table "mort"
+#' \describe{
+#'   \item{code}{code used in the mortality table}
+#'   \item{detailed}{detailed ICD-10 code list}
+#'   \item{cause}{general cause of death}
+#' }
+#'
+#' @keywords datasets
+#'
+#' @source \href{https://www.who.int/healthinfo/statistics/mortality_rawdata/en/}{WHO Mortality Database}
+#'
+#' @examples
+#' data(dd_icd10)
+#' head(dd_icd10)
+"dd_icd10"
